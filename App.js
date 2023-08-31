839d9286b25d1302ea9bf57f943704964ef68741
Ugo28 <ogliugo@gmail.com>
2023-08-31 09:25:53 +0200

    test

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Home from './pages/Home';
+import PageAccueil from './pages/PageAccueil';
+
+const App = () => {
+  return (
+    <Home />
+  );
+};
+
+export default App;
