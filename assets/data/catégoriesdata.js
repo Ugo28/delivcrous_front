3b8d10b795bad46a6f684de620c864d60b4a5815
Ugo28 <ogliugo@gmail.com>
2023-09-04 14:37:13 +0200

    filtre catégorie

--- assets/data/catégoriesdata.js
+++ assets/data/catégoriesdata.js
@@ -10,35 +10,44 @@ const categoriesdata = [
         image:bolognaise,
         title: "Pâtes bolognaise",
         description:"Délicieuse pâtes avec sa viande tendre et sa sauce onctueuse.",
-        prix: "14", 
+        prix: "14",
+        category:'pâtes'
     },
     {
         id:2,
         image:tacos,
         title: "Tacos",
         description:"Giga tacos 3 viandes.",
-        prix: "9", 
+        prix: "9",
+        category:'Tacos'
+
     },
     {
         id:3,
         image: pizza,
         title: "Margherita",
         description:"Incroyable pizza avec sa mozzarella moeulleuse et sa sauce tomate onctueuse.",
-        prix: "12", 
+        prix: "12",
+        category:'pizza'
+
     },
     {
         id:4,
         image: sushi,
         title: "Sushi",
         description:"Riz savoureux avec saumon frais.",
-        prix: "8", 
+        prix: "8",
+        category:'asiatique'
+
     },
     {
         id:5,
         image: burger,
         title: "Burger",
         description:"Excellent pain de boulanger avec son steack haché angus.",
-        prix: "12", 
+        prix: "12",
+        category:'burger'
+
     },
 ]
 
--- pages/Home.js
+++ pages/Home.js
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text, Image, StyleSheet, ScrollView, TouchableOpacity } from 'react-native';
 import categoriesdata from '../assets/data/catégoriesdata';
 import { useNavigation } from '@react-navigation/native';
+import { Feather } from '@expo/vector-icons';
+import { TextInput } from 'react-native';
+
+
 
 export default function Home() {
 
+  const [selectedCategory, setSelectedCategory] = useState(null);
+
+  const categories = ["pâtes", 'Tacos', "pizza", "asiatique", "burger"];
+
+
+  const [cartCount, setCartCount] = useState(0);
+
   const renderCategoriesItem = ({ item }) => {
     return (
-      <View style={styles.categoryCard }>
+      <View style={styles.categoryCard}>
         <Image source={item.image} style={styles.categoryImage} />
         <TouchableOpacity style={styles.categoryInfo} onPress={() => handleCategoryPress(item)}>
           <Text style={styles.categoryTitle}>{item.title}</Text>
@@ -17,7 +28,7 @@ export default function Home() {
         <TouchableOpacity
           style={styles.addButton}
           onPress={() => {
-            // Ajoutez ici le code que vous souhaitez exécuter lorsque le bouton "+" est pressé.
+            setCartCount(cartCount + 1);
           }}
         >
           <Text style={styles.addButtonText}>+</Text>
@@ -31,19 +42,58 @@ export default function Home() {
     navigation.navigate('DataDetails', { item });
   };
 
+  const handleCartPress = () => {
+    navigation.navigate('Cart');
+  };
+
+  const handleCategorySelect = (category) => {
+    setSelectedCategory(category);
+  };
+
+  const filteredCategoriesData = selectedCategory
+  ? categoriesdata.filter((item) => item.category === selectedCategory)
+  : categoriesdata;
+
+
   return (
     <View style={styles.container}>
+      <View style={styles.categoryButtons}>
+        {categories.map((category) => (
+          <TouchableOpacity
+            key={category}
+            style={[
+              styles.categoryButton,
+              selectedCategory === category && styles.selectedCategoryButton,
+            ]}
+            onPress={() => handleCategorySelect(category)}
+          >
+            <Text
+              style={[
+                styles.categoryButtonText,
+                selectedCategory === category && styles.selectedCategoryButtonText,
+              ]}
+            >
+              {category}
+            </Text>
+          </TouchableOpacity>
+        ))}
+      </View>
+
       <ScrollView>
         {/* Popular */}
         <View style={styles.popularWrapper}>
           <Text style={styles.categoriestext}>Carte</Text>
-          {categoriesdata.map(item => (
+          {filteredCategoriesData.map(item => (
             <View key={item.id} style={styles.popularCardWrapper}>
               {renderCategoriesItem({ item })}
             </View>
           ))}
         </View>
       </ScrollView>
+      <TouchableOpacity style={styles.cartButton} onPress={handleCartPress}>
+        <Feather name="shopping-cart" size={24} color="black" />
+        {cartCount > 0 && <View style={styles.cartCounter}><Text style={styles.cartCounterText}>{cartCount}</Text></View>}
+      </TouchableOpacity>
     </View>
   );
 }
@@ -94,7 +144,7 @@ const styles = StyleSheet.create({
     resizeMode: 'contain',
   },
   categoryInfo: {
-    paddingLeft:10,
+    paddingLeft: 10,
     flex: 1, // Pour occuper tout l'espace disponible à gauche de l'image
   },
   categoryTitle: {
@@ -104,17 +154,17 @@ const styles = StyleSheet.create({
   },
   categoryDescr: {
     paddingTop: 10,
-    paddingBottom:5,
+    paddingBottom: 5,
 
     fontSize: 15,
     color: '#777',
   },
   categoriePrice: {
     paddingTop: 5,
-    paddingBottom:10,
-    left:0,
+    paddingBottom: 10,
+    left: 0,
     fontSize: 20,
-    fontWeight:'bold',
+    fontWeight: 'bold',
     color: '#777',
   },
   addButton: {
@@ -125,7 +175,7 @@ const styles = StyleSheet.create({
     width: 85,
     height: 40,
     borderTopLeftRadius: 10,
-    borderBottomRightRadius:10,
+    borderBottomRightRadius: 10,
     justifyContent: 'center',
     alignItems: 'center',
   },
@@ -134,4 +184,58 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     color: 'black',
   },
+  cartButton: {
+    position: 'absolute',
+    bottom: 20, // Ajustez la position verticale selon vos préférences
+    left: 20,   // Ajustez la position horizontale selon vos préférences
+    backgroundColor: '#FF9900',
+    width: 60,
+    height: 60,
+    borderRadius: 100,
+    justifyContent: 'center',
+    alignItems: 'center',
+    zIndex: 2, // Assurez-vous que le bouton est au-dessus du contenu de la ScrollView
+  },
+  cartCounter: {
+    position: 'absolute',
+    top: -5,
+    right: -5,
+    backgroundColor: 'red',
+    width: 20,
+    height: 20,
+    borderRadius: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    zIndex: 3,
+  },
+
+  cartCounterText: {
+    color: 'white',
+    fontSize: 12,
+    fontWeight: 'bold',
+  },
+  categoryButtons: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    marginTop: 10,
+  },
+  categoryButton: {
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    borderRadius: 20,
+    backgroundColor: "#F5F5F5",
+    borderWidth: 1,
+    borderColor: "#E0E0E0",
+  },
+  selectedCategoryButton: {
+    backgroundColor: "#FFC700",
+  },
+  categoryButtonText: {
+    fontSize: 16,
+    fontWeight: "bold",
+    color: "#333",
+  },
+  selectedCategoryButtonText: {
+    color: "white",
+  },  
 });
