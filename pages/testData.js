a68408fd2c7f1bcd6015bd0be44cd9d08b29dcc3
Kalyg <122315970+M8317@users.noreply.github.com>
2024-02-01 14:54:47 +0100

    Create testData.js

---
+++ pages/testData.js
@@ -0,0 +1,13 @@
+// testData.js
+export const mockPlatsData = [
+    {
+      id: 1,
+      title: 'Plat 1',
+      description: 'Description du plat 1',
+      prix: 10.99,
+      image: 'url_de_l_image_1.jpg',
+      category: 'Catégorie 1',
+    },
+    // Ajoutez d'autres plats fictifs si nécessaire
+  ];
+  
